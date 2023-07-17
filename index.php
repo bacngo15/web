7eac27cece36d3fe4a63ee84931876fa49d8a332
bacngo15 <133933326+bacngo15@users.noreply.github.com>
2023-07-17 14:59:35 +0700

    change

--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php
-    include "../config/config.php";
-    if(isset($_POST["txtDN"])){
-        if(($_POST["txtUser"] == "")|| ($_POST["txtPass"] == "")){
-            echo "<script>alert('chưa nhập đầy đủ thông tin');</script>";
-        }else{
-            
+include "../config/config.php";
+if (isset($_POST["txtDN"])) {
+    if ($_POST["txtUser"] == "" || $_POST["txtPass"] == "") {
+        echo "<script>alert('Chưa nhập đầy đủ thông tin');</script>";
+    } else {
 
-            $stmt = sqlsrv_query( $conn,"SELECT * FROM Admin WHERE account = '".$_POST["txtUser"]."' and password = '".$_POST["txtPass"]."' ", 
-            array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+        $stmt = sqlsrv_query($conn, "SELECT * FROM Admin WHERE account = '" . $_POST["txtUser"] . "' and password = '" . $_POST["txtPass"] . "' ",
+            array(), array("Scrollable" => SQLSRV_CURSOR_KEYSET));
 
-            $row_count = sqlsrv_num_rows( $stmt );
-            if($row_count >0){
-                header("location:list.php"); 
-            }
-            else{
-                echo "<script>alert('mật khẩu hoặc tài khoản chưa chính xác!');</script>";
-            }
-            
+        $row_count = sqlsrv_num_rows($stmt);
+        if ($row_count > 0) {
+            header("location:list.php");
+        } else {
+            echo "<script>alert('Mật khẩu hoặc tài khoản chưa chính xác!');</script>";
         }
     }
+}
 ?>
-<table border=1>
-    
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Đăng Nhập Admin</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Đăng nhập trang quản trị</h1>
         <form action="index.php" method="post">
-            <tr>
-                <th>Tên Đăng Nhập:</th><td><input type="text" name="txtUser" /></td>
-            </tr>
-            <tr>
-                <th> Mật Khẩu:</th><td><input type="text" name="txtPass" /></td>
-            </tr>
-            <tr>
-                <th colspan="2"><input type="submit" name="txtDN" value="Đăng Nhập" /></th>
-            </tr>
+            <label for="txtUser">Tên Đăng Nhập:</label>
+            <input type="text" name="txtUser" id="txtUser" required>
+            <label for="txtPass">Mật Khẩu:</label>
+            <input type="password" name="txtPass" id="txtPass" required>
+            <input type="submit" name="txtDN" value="Đăng Nhập">
         </form>
-    
-</table>
-
+    </div>
+</body>
+</html>
--- list.php
+++ list.php
@@ -1,2 +1,11 @@
-<a href="listsp.php">Quản Lí Sản Phẩm</a>
-<a href="listorder.php">Quản Lí Đơn Hàng</a>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Trang quản lý</title>
+    <link rel="stylesheet" href="list.css">
+</head>
+<body>
+    <a href="listsp.php">Quản Lí Sản Phẩm</a>
+    <a href="listorder.php">Quản Lí Đơn Hàng</a>
+</body>
+</html>
